fad612733d7df82d688f09c74dc40da7518c0a30
sjyan <i@yanshengjia.com>
2019-08-06 17:27:20 +0800

    add lc 118

---
+++ leetcode/Dynamic Programming/118. Pascal's Triangle.py
@@ -0,0 +1,39 @@
+"""
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+
+Solution:
+DP
+dp[i][j] = dp[i-1][j-1] + dp[i-1][j], 0<j<len-1
+"""
+
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        dp = [[1]]
+        for i in range(1, numRows):
+            line = [1 for j in range(i+1)]
+            for j in range(1, i):
+                line[j] = dp[i-1][j-1] + dp[i-1][j]
+            dp.append(line)
+        return dp
