87ae35b760a47b60cfc044002e4d98d8ed2e9303
sjyan <i@yanshengjia.com>
2019-08-31 13:30:36 -0400

    add lc 1173

---
+++ leetcode/SQL/1173. Immediate Food Delivery I.sql
@@ -0,0 +1,66 @@
+"""
+Table: Delivery
+
++-----------------------------+---------+
+| Column Name                 | Type    |
++-----------------------------+---------+
+| delivery_id                 | int     |
+| customer_id                 | int     |
+| order_date                  | date    |
+| customer_pref_delivery_date | date    |
++-----------------------------+---------+
+delivery_id is the primary key of this table.
+The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+ 
+
+If the preferred delivery date of the customer is the same as the order date then the order is called immediate otherwise it's called scheduled.
+
+Write an SQL query to find the percentage of immediate orders in the table, rounded to 2 decimal places.
+
+The query result format is in the following example:
+
+Delivery table:
++-------------+-------------+------------+-----------------------------+
+| delivery_id | customer_id | order_date | customer_pref_delivery_date |
++-------------+-------------+------------+-----------------------------+
+| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+| 2           | 5           | 2019-08-02 | 2019-08-02                  |
+| 3           | 1           | 2019-08-11 | 2019-08-11                  |
+| 4           | 3           | 2019-08-24 | 2019-08-26                  |
+| 5           | 4           | 2019-08-21 | 2019-08-22                  |
+| 6           | 2           | 2019-08-11 | 2019-08-13                  |
++-------------+-------------+------------+-----------------------------+
+
+Result table:
++----------------------+
+| immediate_percentage |
++----------------------+
+| 33.33                |
++----------------------+
+The orders with delivery id 2 and 3 are immediate while the others are scheduled.
+
+
+Solution:
+ROUND to 2 decimal places
+SELECT ROUND(135.375, 2);
+
+CASE 后需要加 END
+
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    WHEN conditionN THEN resultN
+    ELSE result
+END;
+"""
+
+
+# Write your MySQL query statement below
+SELECT 
+ROUND(SUM(CASE 
+          WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 
+          END)
+      *100 / COUNT(DISTINCT delivery_id), 2)
+AS immediate_percentage
+FROM Delivery
+
