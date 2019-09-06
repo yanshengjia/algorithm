62e382eb1d0f0f8f2c21aab8f27060cd59dbfa9e
sjyan <i@yanshengjia.com>
2019-09-06 08:56:06 -0400

    add lc 48

---
+++ leetcode/Array/48. Rotate Image.py
@@ -0,0 +1,88 @@
+"""
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+
+
+Solution:
+1. Transpose and reverse row
+2. Rotate four rectangles
+"""
+
+
+# Transpose and reverse row
+# Time: O(N^2), >45%, where N is the length of side of matrix
+# Space: O(1)
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        row, col = len(matrix), len(matrix[0])
+        # transpose
+        for i in range(row):
+            for j in range(col):
+                if i < j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # reverse row
+        for i in range(row):
+            matrix[i] = matrix[i][::-1]
+        return matrix
+
+
+# Rotate 4 rectangles
+# Time: O(N^2), where N is the length of side of matrix
+# Space: O(1)
+class Solution:
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])        
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
