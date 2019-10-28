c2df5ca3543925f38553d2f7801c9f110b6daffc
sjyan <i@yanshengjia.com>
2019-10-27 22:25:50 -0400

    add lomuto partition

--- leetcode/Sort/Quick Sort.py
+++ leetcode/Sort/Quick Sort.py
@@ -1,18 +1,30 @@
-def quicksort(l, start, end):
-    if start > end:
-        return
-    left = start
-    right = end
-    pivot = l[left]
-    while left < right:
-        # 让右边游标往左移动，目的是找到小于 pivot 的值，放到 left 游标位置
-        while left < right and l[right] >= pivot:
-            right -= 1
-        l[left] = l[right]
-        # 让左边游标往右移动，目的是找到大于 pivot 的值，放到 right 游标位置
-        while left < right and l[left] < pivot:
-            left += 1
-        l[right] = l[left]
-    l[left] = pivot
-    quicksort(l, start, left-1)
-    quicksort(l, left, end)
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-  
+# @author: Shengjia Yan
+# @date: 2019-10-27 Sunday
+# @email: i@yanshengjia.com
+# Copyright @ Shengjia Yan. All Rights Reserved.
+
+
+def lomuto_partition(nums, lo, hi):
+    pivot = nums[hi]    # last element as pivot
+    i = lo
+    for j in range(lo, hi):
+        if nums[j] < pivot:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1  # left side of i are smaller than pivot
+    nums[i], nums[hi] = nums[hi], nums[i]
+    return i
+
+
+def quick_sort(nums, lo, hi):
+    if lo < hi:
+        pivot = lomuto_partition(nums, lo, hi)
+        quick_sort(nums, lo, pivot - 1)
+        quick_sort(nums, pivot + 1, hi)
+    return nums
+
+
+if __name__ == "__main__":
+    nums = [1, 3, 6, 7, 2, 4, 5, 8]
+    print(quick_sort(nums, 0, len(nums)-1))
