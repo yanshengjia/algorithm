84a825ba835963210f60d9cb8e3f669b002ec2ae
sjyan <i@yanshengjia.com>
2019-11-21 23:46:48 -0500

    add lc 704

---
+++ leetcode/Search/704. Binary Search.py
@@ -0,0 +1,46 @@
+"""
+Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Note:
+
+You may assume that all elements in nums are unique.
+n will be in the range [1, 10000].
+The value of each element in nums will be in the range [-9999, 9999].
+"""
+
+
+# Binary Search
+# Time: O(logN), N is the length of nums
+# Space: O(1)
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            mid = (i + j) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                j = mid - 1
+            else:
+                i = mid + 1
+        return -1
+
