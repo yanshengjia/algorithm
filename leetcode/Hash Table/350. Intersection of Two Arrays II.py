6b19bbb6bf52a576c511736e1bd195109434fc3e
严晟嘉 <sj@SJ-MacBook-Pro-16.local>
2019-12-31 15:11:52 -0500

    add lc 350

---
+++ leetcode/Hash Table/350. Intersection of Two Arrays II.py
@@ -0,0 +1,44 @@
+"""
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+
+Solution:
+Hashtable
+"""
+
+
+# Hashtable
+# Time: O(m+n), where m, n are the numbers of number in nums1 and nums2
+# Space: O(m+n)
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        d2 = dict()
+        for num in nums2:
+            d2[num] = d2.get(num, 0) + 1
+        
+        for num in nums1:
+            if num in d2 and d2[num] > 0:
+                res.append(num)
+                d2[num] -= 1
+        return res
+                
+        
