213a4f2e2ab8b58b4c4660ac4b2505e846c20281
sjyan <i@yanshengjia.com>
2020-02-08 16:54:02 -0500

    add lc 824

---
+++ leetcode/String/824. Goat Latin.py
@@ -0,0 +1,58 @@
+"""
+A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+The rules of Goat Latin are as follows:
+
+If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+For example, the word 'apple' becomes 'applema'.
+ 
+If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+ 
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+Return the final sentence representing the conversion from S to Goat Latin. 
+
+
+Example 1:
+
+Input: "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+Example 2:
+
+Input: "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ 
+
+Notes:
+S contains only uppercase, lowercase and spaces. Exactly one space between each word.
+1 <= S.length <= 150.
+
+
+Solution:
+Simulation
+"""
+
+
+# String Manipulation
+# Time: O(N), where N is the length of string
+# Space: O(N)
+class Solution:
+    def toGoatLatin(self, S: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        words = S.split()
+        res = []
+        index = 1
+        
+        for word in words:
+            if word[0].lower() not in vowels:
+                first = word[0]
+                word = word[1:]
+                word += first
+            word += 'ma'
+            word += 'a' * index
+            index += 1
+            res.append(word)
+        return ' '.join(res)
