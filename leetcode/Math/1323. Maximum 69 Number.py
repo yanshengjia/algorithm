2404eb6d655efe95ad5d287b85f013f9ef45997f
sjyan <i@yanshengjia.com>
2020-12-18 20:55:34 -0500

    add lc 1323

---
+++ leetcode/Math/1323. Maximum 69 Number.py
@@ -0,0 +1,48 @@
+"""
+Given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+
+Example 1:
+
+Input: num = 9669
+Output: 9969
+Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666. 
+The maximum number is 9969.
+
+Example 2:
+Input: num = 9996
+Output: 9999
+Explanation: Changing the last digit 6 to 9 results in the maximum number.
+
+Example 3:
+Input: num = 9999
+Output: 9999
+Explanation: It is better not to apply any change.
+
+
+Solution:
+Convert digits to string.
+Change the leftmost 6 to 9.
+"""
+
+
+# Time: O(N), N = num size
+# Space: O(N)
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = str(num)
+        quota = 1
+        res = ''
+        for n in num_str:
+            if n == '6' and quota > 0:
+                res += '9'
+                quota -= 1
+            else:
+                res += n
+        return int(res)
