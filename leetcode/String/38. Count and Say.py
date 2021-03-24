16aa3f1460a3d9beff6586897cc888352fa834c8
Shengjia Yan <yshengji@amazon.com>
2021-03-24 00:26:51 -0400

    add lc 38

---
+++ leetcode/String/38. Count and Say.py
@@ -0,0 +1,62 @@
+"""
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+For example, the saying and conversion for digit string "3322251":
+https://assets.leetcode.com/uploads/2020/10/23/countandsay.jpg
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+
+Example 1:
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+
+Example 2:
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+
+
+Constraints:
+* 1 <= n <= 30
+
+
+Solution:
+Iteratively use previous string to generate subsequent string.
+Scan the string and count the times of each char occurs.
+"""
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        res = "1"
+        for i in range(2, n+1):
+            res = self.transform(res)
+        return res
+    
+    # TC: O(N), N = len(s)
+    def transform(self, s: str) -> str:
+        char = s[0]
+        count = 1
+        res = ""
+        for i in range(1, len(s)):
+            if s[i] == char:
+                count += 1
+            else:
+                res += str(count) + char
+                char = s[i]
+                count = 1
+        res += str(count) + char
+        return res
+        
